785b9dd01223643ed84cc0cfa5b1f3c21fb27a7c
autumnlay <autumn.lay0@gmail.com>
2022-01-05 20:51:54 -0700

    knights

---
+++ knights/dbSetup.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS accounts(
+  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name varchar(255) COMMENT 'User Name',
+  email varchar(255) COMMENT 'User Email',
+  picture varchar(255) COMMENT 'User Picture'
+) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS knights(
+  id INT AUTO_INCREMENT NOT NULL primary key COMMENT '',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name TEXT NOT NULL COMMENT 'name of the Knight' clan TEXT NOT NULL COMMENT 'where the knight is from' FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
